d56cca944a92c36fe91dd1f89487aae9b6b0dc00
Francesco Zucaro <francesco.zucaro@waveinformatica.com>
2018-03-23 17:26:13 +0100

    Aggiornamento analisi e procedura PattiParasociali

--- OrganiSociali/SK_F2_FLUSSI.F2_EXP_OrganiSociali.sql
+++ OrganiSociali/SK_F2_FLUSSI.F2_EXP_OrganiSociali.sql
@@ -19,11 +19,10 @@ Parametro dest: indica il destinatario per cui viene generato export
 Valori: CI,FITP,OPA
 */
 CREATE PROCEDURE [SK_F2_FLUSSI].[F2_EXP_OrganiSociali]
-@dataEstrazione date, @dest char(5), @outputNum int OUTPUT, @outputMsg nvarchar(500) OUTPUT
+@dataEstrazione date, @destinatario nvarchar(5), @outputNum int OUTPUT, @outputMsg nvarchar(500) OUTPUT
 WITH EXEC AS CALLER
 AS
 BEGIN
-  declare @idSegnalante         int
   declare @sndgSegnalante       nvarchar(16)
   declare @contesto             nvarchar(2)
   declare @idOperazione         int
@@ -53,10 +52,9 @@ BEGIN
   declare @dataDecorrenza       date
   declare @dataFine             date
   declare @motivoCessazione     nvarchar(250)
+  declare @pesoOrgano           int
+  declare @pesoIncarico         int
   
-  declare @pesoOrgano			int
-  declare @pesoIncarico			int
-
   set @outputNum = 0
 	set @outputMsg = 'OK'
 
@@ -64,25 +62,15 @@ BEGIN
   
   BEGIN TRY
   
-  ---- Verifico esistenza ABI 
-  --SELECT @esisteABI = COUNT(*) FROM SK_F2.F2_D_Banche where ABI_BANCA = @abi
-  --IF @esisteABI > 0 
-  --BEGIN
     
       -- Se esiste già un'estrazione per la stessa data e la stessa segnalante,
       -- cancello i dati precedenti
-				  
       DELETE FROM SK_F2_FLUSSI.F2_T_EXP_OrganiSociali
        WHERE Data_estrazione = @dataEstrazione
-	         AND Destinatario = @dest
-         --AND ABI_Segnalante = @abi
-
-      SELECT @idSegnalante = p.ID, @sndgSegnalante = p.SNDG, @contesto = b.CONT
-        FROM SK_F2.F2_D_Banche b, SK_F2.F2_T_Persona p
-       WHERE p.SNDG = b.SNDG_BANCA 
-         --AND b.ABI_BANCA = @abi
-         AND p.Data_Fine IS NULL
-         
+         AND Destinatario = @destinatario
+	-- eliminata logica segnalante che utilizza abi
+      
+   
       -- Recupero Dati Capogruppo
       SELECT @idPersonaCG = p.ID
         FROM SK_F2.F2_D_Banche b, SK_F2.F2_T_Persona p
@@ -121,35 +109,25 @@ BEGIN
 				ISNULL(esp.EsecutivoFitProper,0),
 				ISNULL(esp.Indipendenza,0), mac.ID, mac.Descrizione, 
 				ISNULL(esp.Data_Decorrenza, esp.Data_Inizio), ISNULL(esp.Data_Fine, '01/01/1900'),
-				case when esp.Data_Fine is null then '' else case when esp.ID_Motivo_Cessazione = 1 then ISNULL(esp.Motivo_Cessazione, '') else mot.Descrizione end end,
-				orgd.descrizione as descrizione_organo,
-				incd.descrizione as descrizione_incarico,
-				orgd.Peso as peso_organo,
-				orginc.Peso as peso_incarico
+				case when esp.Data_Fine is null then '' else case when esp.ID_Motivo_Cessazione = 1 then ISNULL(esp.Motivo_Cessazione, '') else mot.Descrizione end end
            FROM SK_F2.F2_T_Organi_Societa org, SK_F2.F2_T_Incarichi_Societa inc,
-                SK_F2.F2_T_Esponenti esp, SK_F2.F2_D_Macrocategorie_Organo mac, SK_F2.F2_D_Motivi_Cessazione mot,
-				[SK_F2].[F2_D_Organi] orgd,[SK_F2].[F2_D_Organi_Incarichi] orginc,[SK_F2].[F2_D_Incarichi] incd
+                SK_F2.F2_T_Esponenti esp, SK_F2.F2_D_Macrocategorie_Organo mac, SK_F2.F2_D_Motivi_Cessazione mot
           WHERE org.ID_Operazione = @idOperazione
             AND @dataEstrazione between org.Data_Inizio and ISNULL(org.Data_Fine, '31/12/9999')
             AND (org.Cancellata IS NULL or org.Cancellata = 0)
             AND inc.ID_Organo_Societa = org.ID
             AND @dataEstrazione between inc.Data_Inizio and ISNULL(inc.Data_Fine, '31/12/9999')
-			AND orgd.ID = org.ID_Organo
             AND (inc.Cancellata IS NULL or inc.Cancellata = 0)
             AND esp.ID_Incarico_Societa = inc.ID
-			AND orginc.ID_Incarico = inc.ID_Incarico
-			AND incd.ID = orginc.ID_Incarico
             AND @dataEstrazione between esp.Data_Inizio and ISNULL(esp.Data_Fine, '31/12/9999')
             AND (esp.Cancellata IS NULL or esp.Cancellata = 0)
 			AND mac.ID = org.ID_Macro_Categoria
 			AND (esp.Data_Fine is null or mot.ID = esp.ID_Motivo_Cessazione)
         
         OPEN Esponenti_CUR
-        FETCH NEXT FROM Esponenti_CUR 
-				INTO @idPF, @idSocietaGruppo, @idOrgano, 
-					@idIncarico,@esecutivoFitProper,@indipendenza, 
-					@codMacrocat, @descMacrocat, @dataDecorrenza, 
-					@dataFine, @motivoCessazione,@descOrgano,@descIncarico,@pesoOrgano,@pesoIncarico
+        FETCH NEXT FROM Esponenti_CUR INTO @idPF, @idSocietaGruppo, @idOrgano, @idIncarico, 
+						@esecutivoFitProper,
+						@indipendenza, @codMacrocat, @descMacrocat, @dataDecorrenza, @dataFine, @motivoCessazione
       
         WHILE (@@FETCH_STATUS = 0)
         BEGIN 
@@ -166,8 +144,33 @@ BEGIN
                AND pf.Data_Fine IS NULL
             set @codFiscalePers = ISNULL(@codFiscalePers,'')
             set @nominativo = ISNULL(@nominativo,'')
-			set @sndgEsponente = ISNULL(@sndgEsponente,'')
+			      set @sndgEsponente = ISNULL(@sndgEsponente,'')
                
+            -- Ricavo Descrizione Organo
+            set @descOrgano = NULL
+            set @pesoOrgano = 0
+            SELECT @descOrgano = ISNULL(o.Descrizione,''),
+                   @pesoOrgano = ISNULL(o.Peso,0)
+              FROM SK_F2.F2_D_Organi o
+             WHERE o.ID = @idOrgano
+            set @descOrgano = ISNULL(@descOrgano,'')
+            set @pesoOrgano = ISNULL(@pesoOrgano,0)
+            
+            -- Ricavo Descrizione Incarico
+            set @descIncarico = NULL
+            SELECT @descIncarico = ISNULL(i.Descrizione,'')
+              FROM SK_F2.F2_D_Incarichi i
+             WHERE i.ID = @idIncarico
+            set @descIncarico = ISNULL(@descIncarico,'')
+
+            -- Ricavo Peso Incarico
+            set @pesoIncarico = 0
+            SELECT @pesoIncarico = ISNULL(oi.Peso,0)
+              FROM SK_F2.F2_D_Organi_Incarichi oi
+             WHERE oi.ID_Organo = @idOrgano
+               AND ID_Incarico = @idIncarico
+            set @pesoIncarico = ISNULL(@pesoIncarico,0)
+           
             -- Flag Designatore
             set @flagDesignatore = '0'
             IF @idSocietaGruppo = @idPersonaCG
@@ -177,6 +180,7 @@ BEGIN
             
             INSERT INTO SK_F2_FLUSSI.F2_T_EXP_OrganiSociali (
                Data_estrazione
+              ,Destinatario
               ,ABI_Segnalante
               ,SNDG_Partecipata
               ,Codice_Istituto
@@ -189,20 +193,21 @@ BEGIN
               ,Flag_Designatore
               ,Descrizione_Incarico
               ,Descrizione_Organo
-			  ,EsecutivoFitProper
-			  ,Indipendenza
-			  ,SNDG_Esponente
-			  ,Codice_Macrocategoria
-			  ,Descrizione_Macrocategoria
-			  ,Data_Decorrenza_Esponente
-			  ,Data_Fine_Esponente
-			  ,Motivo_Cessazione_Esponente
+			        ,EsecutivoFitProper
+			        ,Indipendenza
+			        ,SNDG_Esponente
+			        ,Codice_Macrocategoria
+			        ,Descrizione_Macrocategoria
+			        ,Data_Decorrenza_Esponente
+			        ,Data_Fine_Esponente
+			        ,Motivo_Cessazione_Esponente
+              ,Peso_Organo
+              ,Peso_Incarico
               ,Flag_Scarto
               ,Motivo_Scarto
-			  ,Peso_Organo
-			  ,Peso_Incarico
             ) VALUES (
                @dataEstrazione  -- Data_estrazione - date
+              ,@destinatario -- Sistema destinatario nvarchar(5)
               ,'00000' -- ABI_Segnalante - nvarchar(5)
               ,@sndgPartecipata -- SNDG_Partecipata - nvarchar(16)
               ,@contesto -- Codice_Istituto - nvarchar(2)
@@ -215,24 +220,23 @@ BEGIN
               ,@flagDesignatore  -- Flag_Designatore - char(1)
               ,@descIncarico -- Descrizione_Incarico - nvarchar(50)
               ,@descOrgano -- Descrizione_Organo - nvarchar(50)
-			  ,@esecutivoFitProper -- Flag esecutivo Fit&Proper
-			  ,@indipendenza -- Flag indipendenza esponente
-			  ,@sndgEsponente -- SNDG esponente
-			  ,@codMacrocat -- Codice macrocategoria organo
-			  ,@descMacrocat -- Codice macrocategoria organo
-			  ,convert(nvarchar, @dataDecorrenza, 112) -- Data decorrenza esponente - nvarchar(8)
+      			  ,@esecutivoFitProper -- Flag esecutivo Fit&Proper
+			        ,@indipendenza -- Flag indipendenza esponente
+			        ,@sndgEsponente -- SNDG esponente
+      			  ,@codMacrocat -- Codice macrocategoria organo
+			        ,@descMacrocat -- Codice macrocategoria organo
+      			  ,convert(nvarchar, @dataDecorrenza, 112) -- Data decorrenza esponente - nvarchar(8)
               ,case when @dataFine = '01/01/1900' then '' else convert(nvarchar, @dataFine, 112) end -- Data fine esponente - nvarchar(8)
-			  ,case when @dataFine = '01/01/1900' then '' else @motivoCessazione end  -- Motivo cessazione esponente - nvarchar(250)
+			        ,case when @dataFine = '01/01/1900' then '' else @motivoCessazione end  -- Motivo cessazione esponente - nvarchar(250)
+              ,@pesoOrgano
+              ,@pesoIncarico
               ,@flagScarto   -- Flag_Scarto - int
               ,@motivoScarto -- Motivo_Scarto - nvarchar(2000)
-			  ,@pesoOrgano
-			  ,@pesoIncarico
             ) 
             
             FETCH NEXT FROM Esponenti_CUR INTO @idPF, @idSocietaGruppo, @idOrgano, @idIncarico, 
-						@esecutivoFitProper,
-						@indipendenza, @codMacrocat, @descMacrocat, @dataDecorrenza, @dataFine, @motivoCessazione,
-						@descOrgano,@descIncarico,@pesoOrgano,@pesoIncarico
+						                                   @esecutivoFitProper, @indipendenza, @codMacrocat, @descMacrocat, 
+                                               @dataDecorrenza, @dataFine, @motivoCessazione
         END
         CLOSE Esponenti_CUR
         DEALLOCATE Esponenti_CUR
@@ -249,112 +253,88 @@ BEGIN
          SET Flag_Scarto = 1,
              Motivo_Scarto = 'Codice SNDG non valorizzato.'
        WHERE Data_estrazione = @dataEstrazione
-         --AND ABI_Segnalante = @abi
+         AND Destinatario = @destinatario
          AND (SNDG_Partecipata IS NULL OR LTRIM(RTRIM(SNDG_Partecipata)) = '')
 
       UPDATE SK_F2_FLUSSI.F2_T_EXP_OrganiSociali
          SET Flag_Scarto = 2,
              Motivo_Scarto = 'Nominativo Esponente non valorizzato.'
        WHERE Data_estrazione = @dataEstrazione
-         --AND ABI_Segnalante = @abi
+         AND Destinatario = @destinatario
          AND (Nominativo IS NULL OR LTRIM(RTRIM(Nominativo)) = '')
          
       UPDATE SK_F2_FLUSSI.F2_T_EXP_OrganiSociali
          SET Flag_Scarto = 3,
              Motivo_Scarto = 'Organo non valorizzato.'
        WHERE Data_estrazione = @dataEstrazione
-         --AND ABI_Segnalante = @abi
+         AND Destinatario = @destinatario
          AND (Codice_Organo IS NULL OR Descrizione_Organo IS NULL OR LTRIM(RTRIM(Descrizione_Organo)) = '')
          
       UPDATE SK_F2_FLUSSI.F2_T_EXP_OrganiSociali
          SET Flag_Scarto = 4,
              Motivo_Scarto = 'Incarico non valorizzato.'
        WHERE Data_estrazione = @dataEstrazione
-         --AND ABI_Segnalante = @abi
+         AND Destinatario = @destinatario
          AND (Codice_Incarico IS NULL OR Descrizione_Incarico IS NULL OR LTRIM(RTRIM(Descrizione_Incarico)) = '')
       
-      -- Select Finale da tabella
-	  IF @dest = 'FITP' 
-	  BEGIN
-		  SELECT left(Codice_Istituto + space(2), 2) +
-				 right('0000000000000000' + Ltrim(Rtrim(SNDG_Partecipata)), 16) +
-				 left(Partita_IVA + space(11), 11) +
-				 left(Codice_UIC + space(13), 13) +
-				 left(Nominativo + space(120), 120) +
-				 left(Codice_Fiscale + space(16), 16) +
-				 left(Codice_Incarico + space(7), 7) +
-				 left(Codice_Organo + space(7), 7) +
-				 ISNULL(Flag_Designatore, '0') +
-				 convert(CHAR (8), @dataEstrazione, 112) +
-				 left(Descrizione_Incarico + space(50), 50) +
-				 left(Descrizione_Organo + space(50), 50) +
-				 ISNULL(EsecutivoFitProper, '0') +
-				 ISNULL(Indipendenza, '0') +
-				 right('0000000000000000' + Ltrim(Rtrim(SNDG_Esponente)), 16) +
-				 left(Codice_Macrocategoria + space(10), 10) +
-				 left(Descrizione_Macrocategoria + space(50), 50) +
-				 convert(CHAR (8), Data_Decorrenza_Esponente, 112) +
-				 convert(CHAR (8), Data_Fine_Esponente, 112) +
-				 left(Motivo_Cessazione_Esponente + space(250), 250)
-			FROM SK_F2_FLUSSI.F2_T_EXP_FITP_OrganiSociali
-			WHERE Data_estrazione = @dataEstrazione
-			  AND Flag_Scarto = 0
-        END
-		ELSE IF @dest = 'CI'
-		BEGIN
-			SELECT Codice_Istituto +
-             right('0000000000000000' + Ltrim(Rtrim(SNDG_Partecipata)), 16) +
-             left(Partita_IVA + space(11), 11) +
-             left(Codice_UIC + space(13), 13) +
-             left(Nominativo + space(120), 120) +
-             left(Codice_Fiscale + space(16), 16) +
-             left(Codice_Incarico + space(7), 7) +
-             left(Codice_Organo + space(7), 7) +
-             ISNULL(Flag_Designatore, '0') +
-             convert(char(8), @dataEstrazione, 112) +
-             left(Descrizione_Incarico + space(50), 50) +
-             left(Descrizione_Organo + space(50), 50)
-				FROM SK_F2_FLUSSI.F2_T_EXP_CI_OrganiSociali
-				WHERE Data_estrazione = @dataEstrazione
-				-- AND ABI_Segnalante = @abi
-				AND Flag_Scarto = 0 
-	  END
-       ELSE IF @dest = 'OPA'
-	   BEGIN
-		SELECT left(Codice_Istituto + space(2), 2) +
-				 right('0000000000000000' + Ltrim(Rtrim(SNDG_Partecipata)), 16) +
-				 left(Partita_IVA + space(11), 11) +
-				 left(Codice_UIC + space(13), 13) +
-				 left(Nominativo + space(120), 120) +
-				 left(Codice_Fiscale + space(16), 16) +
-				 left(Codice_Incarico + space(7), 7) +
-				 left(Codice_Organo + space(7), 7) +
-				 ISNULL(Flag_Designatore, '0') +
-				 convert(CHAR (8), @dataEstrazione, 112) +
-				 left(Descrizione_Incarico + space(50), 50) +
-				 left(Descrizione_Organo + space(50), 50) +
-				 ISNULL(EsecutivoFitProper, '0') +
-				 ISNULL(Indipendenza, '0') +
-				 right('0000000000000000' + Ltrim(Rtrim(SNDG_Esponente)), 16) +
-				 left(Codice_Macrocategoria + space(10), 10) +
-				 left(Descrizione_Macrocategoria + space(50), 50) +
-				 convert(CHAR (8), Data_Decorrenza_Esponente, 112) +
-				 convert(CHAR (8), Data_Fine_Esponente, 112) +
-				 left(Motivo_Cessazione_Esponente + space(250), 250),
-				 ISNULL(@PesoOrgano,''),
-				 ISNULL(@PesoIncarico,'')
-				FROM SK_F2_FLUSSI.F2_T_EXP_FITP_OrganiSociali
-				WHERE Data_estrazione = @dataEstrazione
-				  AND Flag_Scarto = 0
-        END
-		ELSE
-			BEGIN
-					-- destinatario non riconosciuto
-				UPDATE SK_F2_FLUSSI.F2_T_OrganiSociali
-				SET Flag_Scarto = 5,
-					Motivo_Scarto = CONCAT('Destinatario non riconosciuto: ',@dest)
-			WHERE Data_estrazione = @dataEstrazione
-	   END      
+    -- Select Finale da tabella in base a destinatario
+    IF @destinatario = 'OPA'
+    BEGIN
+  	  SELECT left(Codice_Istituto + space(2), 2) +
+	           right('0000000000000000' + Ltrim(Rtrim(SNDG_Partecipata)), 16) +
+		      	 left(Partita_IVA + space(11), 11) +
+			       left(Codice_UIC + space(13), 13) +
+			       left(Nominativo + space(120), 120) +
+			       left(Codice_Fiscale + space(16), 16) +
+			       left(Codice_Incarico + space(7), 7) +
+			       left(Codice_Organo + space(7), 7) +
+			       ISNULL(Flag_Designatore, '0') +
+			       convert(CHAR (8), @dataEstrazione, 112) +
+			       left(Descrizione_Incarico + space(50), 50) +
+  		    	 left(Descrizione_Organo + space(50), 50) +
+	  		     ISNULL(EsecutivoFitProper, '0') +
+		         ISNULL(Indipendenza, '0') +
+      			 right('0000000000000000' + Ltrim(Rtrim(SNDG_Esponente)), 16) +
+			       left(Codice_Macrocategoria + space(10), 10) +
+			       left(Descrizione_Macrocategoria + space(50), 50) +
+			       convert(CHAR (8), Data_Decorrenza_Esponente, 112) +
+			       convert(CHAR (8), Data_Fine_Esponente, 112) +
+			       left(Motivo_Cessazione_Esponente + space(250), 250) +
+             right('     ' + Ltrim(Rtrim(convert(nvarchar, Peso_Organo))), 5) +
+             right('     ' + Ltrim(Rtrim(convert(nvarchar, Peso_Incarico))), 5)
+		    FROM SK_F2_FLUSSI.F2_T_EXP_OrganiSociali
+		   WHERE Data_estrazione = @dataEstrazione
+		     AND Destinatario = @destinatario
+		     AND Flag_Scarto = 0    
+    END
+    ELSE
+    BEGIN
+  	  SELECT left(Codice_Istituto + space(2), 2) +
+	           right('0000000000000000' + Ltrim(Rtrim(SNDG_Partecipata)), 16) +
+		      	 left(Partita_IVA + space(11), 11) +
+			       left(Codice_UIC + space(13), 13) +
+			       left(Nominativo + space(120), 120) +
+			       left(Codice_Fiscale + space(16), 16) +
+			       left(Codice_Incarico + space(7), 7) +
+			       left(Codice_Organo + space(7), 7) +
+			       ISNULL(Flag_Designatore, '0') +
+			       convert(CHAR (8), @dataEstrazione, 112) +
+			       left(Descrizione_Incarico + space(50), 50) +
+  		    	 left(Descrizione_Organo + space(50), 50) +
+	  		     ISNULL(EsecutivoFitProper, '0') +
+		         ISNULL(Indipendenza, '0') +
+      			 right('0000000000000000' + Ltrim(Rtrim(SNDG_Esponente)), 16) +
+			       left(Codice_Macrocategoria + space(10), 10) +
+			       left(Descrizione_Macrocategoria + space(50), 50) +
+			       convert(CHAR (8), Data_Decorrenza_Esponente, 112) +
+			       convert(CHAR (8), Data_Fine_Esponente, 112) +
+			       left(Motivo_Cessazione_Esponente + space(250), 250)
+		    FROM SK_F2_FLUSSI.F2_T_EXP_OrganiSociali
+		   WHERE Data_estrazione = @dataEstrazione
+		     AND Destinatario = @destinatario
+		     AND Flag_Scarto = 0    
+    END
+    
  END TRY
 	BEGIN CATCH
 		set @outputNum = -1
@@ -368,6 +348,3 @@ BEGIN
 		COMMIT TRANSACTION;
 
 END
-GO
-
-
--- README.md
+++ README.md
@@ -17,3 +17,13 @@ git commit -m "first commit"
 git remote add origin https://github.com/fzucaro/OPA.git
 git push -u origin master
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ 
+
+Appunti su Git
+==============
+
+Posso bypassare area di stage mediante il comando git commit -a -m “”: in questo
+modo committo tutti i file che inizialmente erano tracciati.
+
+ 
